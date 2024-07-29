e3bf2094ad03c219493f93ed3ff1196617e0e3f9
danielxbeam <laurentiusdanieladekurniawan@gmail.com>
2024-07-29 18:16:41 +0200

    afd.5

--- src/componnents/Benefits.jsx
+++ src/componnents/Benefits.jsx
@@ -22,7 +22,8 @@ const Benefits = () => {
             >
               <div className="relative z-2 flex flex col min-h-[22rem] p-[2.4rem] pointer-events-none">
                 <h5>{item.title}</h5>
-                <p className="body-2 mb-6 text-n-3"> {item.text}</p>
+                <p className="body-2 mb-6 text-n-3">{item.text}</p>
+                <div className="flex items=center mt-auto"></div>
               </div>
             </div>
           ))}
