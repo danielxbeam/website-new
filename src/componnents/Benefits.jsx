d61fc0df310dc03fada100f12cbb966af068e66d
danielxbeam <laurentiusdanieladekurniawan@gmail.com>
2024-07-29 18:33:13 +0200

    afd.7

--- src/componnents/Benefits.jsx
+++ src/componnents/Benefits.jsx
@@ -1,6 +1,7 @@
 import { benefits } from "../constants";
 import Heading from "./Heading";
 import Section from "./Section";
+import { ReactSVG } from "react-svg";
 
 const Benefits = () => {
   return (
@@ -15,15 +16,17 @@ const Benefits = () => {
           {benefits.map((item) => (
             <div
               className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
-              style={{
-                backgroundImage: `url(${item.backgroundUrl})`,
-              }}
               key={item.id}
             >
-              <div className="relative z-2 flex flex col min-h-[22rem] p-[2.4rem] pointer-events-none">
+              <ReactSVG
+                src={item.backgroundUrl}
+                className="absolute top-0 left-0 w-full h-full"
+                style={{ pointerEvents: "none" }}
+              />
+              <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
                 <h5>{item.title}</h5>
                 <p className="body-2 mb-6 text-n-3">{item.text}</p>
-                <div className="flex items=center mt-auto"></div>
+                <div className="flex items-center mt-auto"></div>
               </div>
             </div>
           ))}
